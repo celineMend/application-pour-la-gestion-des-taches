13cf7cad090be9763aa4f17894c9078b34c4f187
celineMend <celine.mendy@unchk.edu.sn>
2024-04-12 21:17:03 +0100

    création de la page de déconnexion des employés

---
+++ déconnexion.php
@@ -0,0 +1,11 @@
+<?php
+// Démarrage de la session
+session_start();
+
+// Destruction de toutes les données de session
+session_destroy();
+
+// Redirection vers la page de connexion
+header("Location: connexion.php");
+exit();
+?>
